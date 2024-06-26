b7cbbbbd2389955dd70ad7d6e9d32db72c767ba6
kodunaiff <kodunaiff@gmail.com>
2024-06-26 10:38:52 +0800

    прикрутил кнопки

--- selfstorage/urls.py
+++ selfstorage/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', render, kwargs={'template_name': 'index.html'}, name='start_page'),
