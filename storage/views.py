03dd98ffcaf872d55b633fbe96352bd48153cc63
kodunaiff <kodunaiff@gmail.com>
2024-06-26 14:18:39 +0800

    update /boxes

--- storage/views.py
+++ storage/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
 def view_products(request):
+    boxes =[
+        {
+            'name':'тест_Новосибирск',
+            'address':'ул. Лермонтова д.15_тест',
+        },
+        {
+            'name':'тест_Иркутск',
+            'address':'ул. Пушкина д.17_тест',
+        },
+    ]
 
     # logic
 
-    return render(request, template_name="boxes_t.html")
+    return render(request, template_name="boxes_t.html", context={'boxes': boxes})
 
 # Create your views here.
